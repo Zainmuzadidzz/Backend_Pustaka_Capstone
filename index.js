0a7d184790d54a03cfeb7085ad51b854a79a5d44
Agusprab <agusbowo363@gmail.com>
2024-06-05 11:22:07 +0700

    fix bug

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(BookRoute);
 app.use(PeminjamanRoute);
 app.use(AuthRoute);
 
-const port = process.env.PORT || 3000;
+const port = process.env.DB_PORT || 3000;
 app.listen(port, () => {
     console.log('Server up and running',port)
 });
