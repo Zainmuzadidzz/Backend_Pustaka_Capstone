14c67e840e090a491619667efb91f95b6451bd8d
Agusprab <agusbowo363@gmail.com>
2024-06-09 03:40:28 +0700

    test change database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-import mysql2 from 'mysql2';
+
 
 dotenv.config();
 
@@ -9,14 +9,18 @@ dotenv.config();
 //     host: process.env.DB_HOST,
 //     dialect: 'mysql'
 // });
-
-const db = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
-    dialectModule: mysql2
-})
+    dialect: "mysql"
+});
+// const db = new Sequelize({
+//     database: process.env.DB_NAME,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql',
+//     dialectModule: mysql2
+// })
 export default db;
